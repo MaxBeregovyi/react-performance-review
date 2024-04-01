653468fe35a060a04ee390361d3358c4e2c086e8
Max <BeregovoyMaksim777@gmail.com>
2024-04-02 01:40:04 +0300

    add creat account form

--- src/components/common/AppHeader.jsx
+++ src/components/common/AppHeader.jsx
@@ -3,7 +3,7 @@ import AuthProvider from '../AuthProvider.jsx';
 
 const AppHeader = () => {
   return (
-    <header className={'container flex items-center justify-between p-4'}>
+    <header className={'container flex items-center justify-between py-4'}>
       <h1 className={'font-playfair-display text-8xl'}>Hello World!</h1>
       <AuthProvider />
     </header>
--- src/routes/RegistrationUserPage.jsx
+++ src/routes/RegistrationUserPage.jsx
@@ -1,10 +1,58 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 const RegistrationUserPage = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState('');
+  const [userPass, setUserPass] = useState('');
+
+  const userReg = () => {
+    createUserWithEmailAndPassword(auth, userEmail, userPass)
+      .then((userData) => {
+        console.log(userData);
+        setUserEmail('');
+        setUserPass('');
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
-    <div>
-      <h2 className={'flex justify-center text-5xl text-blue-300'}>
-        Registration Page
-      </h2>
-    </div>
+    <>
+      <div className={'container'}>
+        <h2 className={'flex justify-center text-5xl text-blue-300'}>
+          Registration Page
+        </h2>
+        <div className={'inline-flex flex-col items-center'}>
+          <input
+            value={userEmail}
+            onChange={(event) => setUserEmail(event.target.value)}
+            className={
+              'mb-2 block rounded-2xl border-2 border-[#24292F] bg-transparent pl-3 text-white'
+            }
+            placeholder={'Email'}
+            type='text'
+            name='email'
+            autoComplete={'username'}
+          />
+          <input
+            value={userPass}
+            onInput={(event) => setUserPass(event.target.value)}
+            className={
+              'mb-2 block rounded-2xl border-2 border-[#24292F] bg-transparent pl-3 text-white'
+            }
+            type='password'
+            name='password'
+            placeholder={'Password'}
+            autoComplete={'current-password'}
+          />
+          <button onClick={userReg} className={'link-style'}>
+            Register
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
